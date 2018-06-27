833b33aae1368f262f706db3c2eb8384199dcfce
Kirill <Nordian89@mail.ru>
2018-06-28 01:25:02 +0300

    dz_students

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+$students= array (
+    array(
+        'name' => 'Мафусаил',
+    ),
+    array(
+        'name' => 'Прокопий',
+    ),
+    array(
+        'name' => 'Феоктист',
+    ),
+);
+
+
+
+foreach ($students as $student) {
+    foreach ($student as $data=>$value) {
+        echo '<div> Студент '.$value;
+        for($i=0; $i < 3; $i++){
+            if (empty($marks)) {
+                $marks=array();
+            };
+            $marks[$i]= rand(1, 5);
+            $srb = round((array_sum($marks)/sizeof($marks)),2);
+        };
+        echo ' имеет '.sizeof($marks).' оценки: '.implode(', ',$marks).'; средний балл- '.$srb.';    </div>';
+    };
+};
+
+
+
+?>
